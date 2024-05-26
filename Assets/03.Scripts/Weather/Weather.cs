ad783bcd348854acabdaec556ea683a3e260c89e
Hyodonjoo <wngyehs99@gmail.com>
2024-05-26 13:30:19 +0900

    Feat: winow server time to weather.cs(윈도우 서버 시간에 기반해 현재 날씨관련 정보 불러옴)

--- Assets/03.Scripts/Weather/Weather.cs
+++ Assets/03.Scripts/Weather/Weather.cs
@@ -6,24 +6,24 @@ using System.Net;
 using System.Net.Http;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using System.Globalization;
 
 public class Weather : MonoBehaviour
 {
     static HttpClient client = new HttpClient();
-
-    // URL과 관련된 상수
+        
     private const string ServiceKey = "b0b3Iuv4C7pVspeadXHv2qkPA75fHpZeklVYsfGYVEbhRk4spDkktlwH4ZBaY80hmCyxjlCduwwE7%2F7DN6BiFQ%3D%3D";
     private const string baseUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
 
-    static void weahter()
+    static void weather(string date, string time)
     {
         string url = baseUrl;
         url += "?ServiceKey=" + ServiceKey;
         url += "&pageNo=1";
         url += "&numOfRows=1000";
-        url += "&dataType=JSON"; // 데이터를 JSON 형식으로 요청합니다.
-        url += "&base_date=20240526";
-        url += "&base_time=0600";
+        url += "&dataType=JSON"; 
+        url += "&base_date=" + date;
+        url += "&base_time=" + time;
         url += "&nx=63";
         url += "&ny=89";
 
@@ -37,7 +37,7 @@ public class Weather : MonoBehaviour
             StreamReader reader = new StreamReader(response.GetResponseStream());
             results = reader.ReadToEnd();
         }
-
+                
         ParseWeatherData(results);
     }
 
@@ -52,20 +52,29 @@ public class Weather : MonoBehaviour
             string category = item["category"].ToString();
             string value = item["obsrValue"].ToString();
 
+            if (category == "PTY") // PTY, REH, RN1, T1H, UUU, VEC, VVV, WSD
+            {
+                Debug.Log("현재 강수형태: " + value);
+            }
+
             // 추출한 정보를 출력합니다.
             Debug.Log("Category: " + category + ", Value: " + value);
-        }
+        }        
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        weahter();
-    }
+        NTPClient ntpClient = new NTPClient();
+        DateTime networkTime = ntpClient.GetNetworkTime();        
 
-    // Update is called once per frame
-    void Update()
-    {
+        string date = networkTime.ToString("yyyyMMdd");
+        string time = networkTime.ToString("HHmm");
 
-    }
+        Debug.Log("Date: " + date);
+        Debug.Log("Time: " + time);
+
+        weather(date, time);
+    }        
 }
