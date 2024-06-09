bad1abc48e6445c13104c466a01dcf616379bca4
gdevhun <wjh9330@naver.com>
2024-06-09 16:30:57 +0900

    updated: cat anim asset 스케일 수정

---
+++ Assets/03.Scripts/MainSceneInitUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainSceneInitUI : MonoBehaviour
+{
+    public TextMeshProUGUI resultMBTIInfoHeadText;
+    public TextMeshProUGUI resultMBTIInfoTailText;
+    private void InitMainSceneUI()
+    {
+        
+    }
+}
