e439432974db12cd2f76b3285e2883ca3a2950d5
gdevhun <wjh9330@naver.com>
2024-06-09 17:26:02 +0900

    feat: catAnim컨트롤러스크립트 추가, animaitor 파라미터& fixduration 수정및연결

---
+++ Assets/03.Scripts/CatAnimController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimController : Singleton<CatAnimController>
+{
+    public enum CatAnimType
+    {
+        CatRun,
+        CatJoy,
+        CatDead,
+        CatJump,
+        CatHit,
+        CatGravitation
+    }
+    private Animator catAnimator;
+    void Start()
+    {
+        catAnimator = GetComponent<Animator>();
+    }
+
+    public void PlayCatAnimation(CatAnimType catAnimType)
+    {
+        catAnimator.SetTrigger(catAnimType.ToString());
+    }
+    /*
+     //에니메이션 작동 테스트 코드
+     
+     private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            catAnimator.SetTrigger("isRun");
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            catAnimator.SetTrigger("isJoy");
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            catAnimator.SetTrigger("isDead");
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            catAnimator.SetTrigger("isGraviation");
+        }
+        else if (Input.GetKey(KeyCode.Alpha5))
+        {
+            catAnimator.SetTrigger("isHit");
+        }
+        else if (Input.GetKey(KeyCode.Alpha6))
+        {
+            catAnimator.SetTrigger("isJump");
+        }
+    }*/
+}
