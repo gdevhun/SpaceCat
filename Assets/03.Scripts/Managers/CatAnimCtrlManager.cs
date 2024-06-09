fcfccef3c50e4d2dd3b5084db557106e18cc3093
gdevhun <wjh9330@naver.com>
2024-06-09 19:36:22 +0900

    fix: 버튼 에니메이션 설정 수정

--- Assets/03.Scripts/Managers/CatAnimCtrlManager.cs
+++ Assets/03.Scripts/Managers/CatAnimCtrlManager.cs
@@ -3,27 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatAnimCtrlManager : Singleton<CatAnimCtrlManager>
+public class CatAnimCtrlManager : MonoBehaviour
 {
     public enum CatAnimType
     {
-        CatRun,
-        CatJoy,
-        CatDead,
-        CatJump,
-        CatHit,
-        CatGravitation
+        isRun,
+        isJoy,
+        isDead,
+        isJump,
+        isHit,
+        isGravitation
     }
-    private Animator catAnimator;
+    
+    public Animator catAnimator;
     void Start()
     {
         catAnimator = GetComponent<Animator>();
     }
 
-    public void PlayCatAnimation(CatAnimType catAnimType)
+    /*public void PlayCatAnimation(CatAnimType catAnimType)
     {
         
         catAnimator.SetTrigger(catAnimType.ToString());
+    }*/
+    
+    public void PlayCatAnimRun()
+    {
+        catAnimator.SetTrigger(CatAnimType.isRun.ToString());
+    }
+    public void PlayCatAnimDead()
+    {
+        catAnimator.SetTrigger(CatAnimType.isDead.ToString());
+    }
+    public void PlayCatAnimJump()
+    {
+        catAnimator.SetTrigger(CatAnimType.isJump.ToString());
+    }
+    public void PlayCatAnimJoy()
+    {
+        catAnimator.SetTrigger(CatAnimType.isJoy.ToString());
+    }
+    public void PlayCatAnimHit()
+    {
+        catAnimator.SetTrigger(CatAnimType.isHit.ToString());
+    }
+    public void PlayCatAnimGravitation()
+    {
+        catAnimator.SetTrigger(CatAnimType.isGravitation.ToString());
     }
     /*
      //에니메이션 작동 테스트 코드
