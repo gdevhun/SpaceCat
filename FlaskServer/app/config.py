4162164782542a321fb41efb75dad36a3483e56a
CoEf <144963869+HooAaam@users.noreply.github.com>
2024-06-16 11:06:48 +0900

    feat: login & config 추가 (Flask 서버)

---
+++ FlaskServer/app/config.py
@@ -0,0 +1,13 @@
+# app/config.py
+
+class Config:
+    FIREBASE_CONFIG = {
+        "apiKey": "AIzaSyCPI4_03fyAqPilT2cq3lQhRYhtm0BMsUs",
+        "authDomain": "ossteamproject-a4ea0.firebaseapp.com",
+        "databaseURL": "https://ossteamproject-a4ea0-default-rtdb.firebaseio.com",
+        "projectId": "ossteamproject-a4ea0",
+        "storageBucket": "ossteamproject-a4ea0.appspot.com",
+        "messagingSenderId": "794886672080",
+        "appId": "1:794886672080:web:310a69b7fd0915c7de98d5",
+        "measurementId": "G-5RP0M42BY7"
+    }
