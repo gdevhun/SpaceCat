b00deb33cbd5e501e76df210f5edf7c2823e1c42
CoEf <144963869+HooAaam@users.noreply.github.com>
2024-06-16 16:09:07 +0900

    Remove: 파일 정리

--- Assets/03.Scripts/FirebaseAuth-Flask/ResponseObject.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class ResponseObject
-{
-    public string status;
-    public string message;
-    public object data;
-}
--- FlaskServer/app/logout.py
+++
@@ -1,9 +0,0 @@
-# app/logout.py
-
-from flask import Blueprint, jsonify
-
-logout_bp = Blueprint('logout', __name__)
-
-@logout_bp.route('/logout', methods=['POST'])
-def logout():
-    return jsonify({"status": "success", "message": "Logged out successfully"}), 200
